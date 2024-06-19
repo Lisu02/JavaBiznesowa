2fe999cd94781692776e9b89279706fe66b22f09
Lisu02 <104763694+Lisu02@users.noreply.github.com>
2024-06-19 18:55:03 +0200

    poprawa data.sql bardzo wazna

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,9 +3,13 @@
 
 
 -- Inserting users data
+TRUNCATE TABLE users,authorities;
+-- usuwac zawartosc tabel wazne!!!!
+
 INSERT INTO users (username, enabled, password) VALUES
 ('user', true, '{noop}test123'),
 ('admin', true, '{noop}test123');
+
 
 INSERT INTO authorities (authority, username) VALUES
 ('ROLE_EMPLOYEE', 'user'),
