eb78c0efd1b2af7de2930585d484b5e2d86e2e0c
Lisu02 <104763694+Lisu02@users.noreply.github.com>
2024-06-19 22:54:25 +0200

    Proste logowanie z wykorzystaniem aspektów

---
+++ src/main/java/com/example/projektJava/aspect/LoggingAspect.java
@@ -0,0 +1,36 @@
+package com.example.projektJava.aspect;
+
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+
+
+   // private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+
+    @Pointcut("execution(* com.example.projektJava.service.*.*(..))")
+    public void applicationPackagePointcut() {
+        // Metoda jest pusta bo to pointcut implementacje w metodach xd
+    }
+
+
+    @Before("applicationPackagePointcut()")
+    public void logBefore() {
+        Logger logger = LoggerFactory.getLogger(this.getClass());
+        logger.info("A method is about to be executed.");
+    }
+
+    @AfterReturning(pointcut = "applicationPackagePointcut()", returning = "result")
+    public void logAfterReturning(Object result) {
+        Logger logger = LoggerFactory.getLogger(this.getClass());
+        logger.info("Metoda została wykonana i zwróciła: " + result);
+    }
+}
--- src/main/java/com/example/projektJava/service/EmailService.java
+++ src/main/java/com/example/projektJava/service/EmailService.java
@@ -1,15 +1,23 @@
 package com.example.projektJava.service;
 
+import org.aspectj.lang.annotation.AfterReturning;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.CompletableFuture;
+
 @Service
 public class EmailService {
 
     private JavaMailSender mailSender;
+
+    private final Logger superLogger = LoggerFactory.getLogger(this.getClass());
+
 
     @Autowired
     public EmailService(JavaMailSender mailSender){
@@ -24,5 +32,12 @@ public class EmailService {
         message.setText(text);
         message.setFrom("serwerOgloszen@example.com"); // Adres nadawcy
         mailSender.send(message);
+        superLogger.info("Mail został wysłany prawidłowo -> " + message);
+    }
+
+
+    public String testowySerwisLogowanie(){
+        superLogger.info("Testowy Log");
+        return "Wynik Testowej Metody";
     }
 }
